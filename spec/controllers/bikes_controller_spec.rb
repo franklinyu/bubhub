acebe5b5237a59db494335b4f0247ad5568c8552
Franklin Yu <franklinyu@hotmail.com>
2015-04-20 17:35:34 -0400

    rspec test (first two)

--- spec/controllers/bikes_controller_spec.rb
+++ spec/controllers/bikes_controller_spec.rb
@@ -2,7 +2,19 @@ require 'spec_helper'
 
 describe BikesController do
   describe 'select bike' do
-    it 'should check the database for the availability for the bike with model method'
+    before :each do
+      @fake_bike = mock('bike 1')
+    end
+    it 'should check that user is logged in' do
+      Transaction.should_receive(:is_logged_in?).and_return(true)
+      Bike.stub(:is_available?).with(@fake_bike).and_return(true)
+      post :select, {bike_id: 10}
+    end
+    it 'should check the database for the availability for the bike with model method' do
+      Transaction.stub(:is_logged_in?).and_return(true)
+      Bike.should_receive(:is_available?).with(@fake_bike).and_return(true)
+      post :select, {bike_id: 10}
+    end
     it 'should redirect user to the checkout page'
     it 'should make the bike information available for the checkout page template'
   end
