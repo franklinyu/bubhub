17d385f508ff7f0d5cdda3f3186f2fa461234986
Benny Mei <meibenny@gmail.com>
2015-04-20 20:57:24 -0300

    removed duplicate user controller file

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,40 +1,37 @@
-class UsersController < ApplicationController
+class UserController < ApplicationController
   def create
-    @user = User.new
-    @user.first_name = params[:user][:first_name]
-    @user.last_name = params[:user][:last_name]
-    @user.bu_id = params[:user][:bu_id]
-    @user.email = params[:user][:email]
-    @user.pin = params[:user][:pin]
-
-    if @user.save
-      @createdUser = User.find_by(bu_id: @user.bu_id)
-      flash[:notice] = " An account for #{@createdUser.first_name} #{@createdUser.last_name} was successfully created"
-      redirect_to root_page_path and return
+    if not (params[:user][:firstName].present? and params[:user][:lastName].present? and
+      params[:user][:bMail].present? and params[:user][:BNumber].present? and
+      params[:user][:pin].present? and params[:user][:phoneNumber].present? and
+      params[:user][:BUCardNumber].present?)
+        flash[:error]= "Please fill in all required fields."
+        redirect_to register_page_path and return
     else
-      flash[:error]= "Please fill in all required fields."
-      redirect_to new_user_path and return
+      @user = User.create!(user_params)
+      flash[:notice] = " An account for #{@user.firstName} #{@user.lastName} was successfully created"
+      redirect_to root_page_path and return
     end
-  end
 
-  def new
-    render "register"
+
+       
+   
   end
 
   def read
   end
 
   def update
+    redirect_to root_page_path
   end
 
   def delete
   end
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :bu_id, :pin)
+    params.require(:user).permit(:firstName, :lastName, :bMail, :BNumber, :pin, :phoneNumber, :BUCardNumber)
   end
 
-  def index
+  def register
     render "register"
   end
 end
