ae054a8339e9e812bb1cd0e01b78d72da6c9e2e3
Franklin Yu <franklinyu@hotmail.com>
2015-04-20 21:31:57 -0300

    new user story for it
    Create new user story for checking whether the user is currently logged in.
    It should be a filter in 'ApplicationController'.

--- spec/controllers/bikes_controller_spec.rb
+++ spec/controllers/bikes_controller_spec.rb
@@ -5,11 +5,6 @@ describe BikesController do
     before :each do
       @fake_bike = mock('bike 1')
     end
-    it 'should check that user is logged in' do
-      Transaction.should_receive(:is_logged_in?).and_return(true)
-      Bike.stub(:is_available?).with(@fake_bike).and_return(true)
-      post :select, {bike_id: 10}
-    end
     it 'should check the database for the availability for the bike with model method' do
       Transaction.stub(:is_logged_in?).and_return(true)
       Bike.should_receive(:is_available?).with(@fake_bike).and_return(true)
