e0d6d112c0f27a2531ed8f0a4f11c8586f98d34c
Franklin Yu <franklinyu@hotmail.com>
2015-04-22 16:52:31 -0400

    use internal ID (master key) for bike

--- spec/controllers/bikes_controller_spec.rb
+++ spec/controllers/bikes_controller_spec.rb
@@ -6,9 +6,9 @@ describe BikesController do
       @fake_bike = mock('bike 1')
     end
     it 'should check the database for the availability for the bike with model method' do
-      Bike.should_receive(:find_by_bike_id).with(10).and_return(@fake_bike)
+      Bike.should_receive(:find_by_id).with(10).and_return(@fake_bike)
       @fake_bike.should_receive(:is_available?).and_return(true)
-      post :select, {bike_id: 10}
+      post :select, {id: 10}
     end
     it 'should redirect user to the checkout page'
     it 'should make the bike information available for the checkout page template'
