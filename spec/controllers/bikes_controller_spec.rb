3129ea914e783b446eff1dfe7006516f00a5c6fa
Franklin Yu <franklinyu@hotmail.com>
2015-04-22 17:18:55 -0400

    second spec case

--- spec/controllers/bikes_controller_spec.rb
+++ spec/controllers/bikes_controller_spec.rb
@@ -8,9 +8,14 @@ describe BikesController do
     it 'should check the database for the availability for the bike with model method' do
       Bike.should_receive(:find_by_id).with(10).and_return(@fake_bike)
       @fake_bike.should_receive(:is_available?).and_return(true)
-      get :checkout, {id: 10}
+      get :check_out, {id: 10}
     end
-    it 'should select the Check Out template for rendering'
+    it 'should select the Check Out template for rendering' do
+      Bike.stub(:find_by_id).with(10).and_return(@fake_bike)
+      @fake_bike.should_receive(:is_available?).and_return(true)
+      get :check_out, {id: 10}
+      response.should render_template('che_ckout')
+    end
     it 'should make the bike information available for the checkout page template'
   end
 end
