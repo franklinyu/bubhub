a815e1e24c32c64075d9cde3c2846c6903ffcf4e
Franklin Yu <franklinyu@hotmail.com>
2015-04-22 17:43:34 -0400

    finish spec

--- spec/controllers/bikes_controller_spec.rb
+++ spec/controllers/bikes_controller_spec.rb
@@ -10,12 +10,18 @@ describe BikesController do
       @fake_bike.should_receive(:is_available?).and_return(true)
       get :check_out, {id: 10}
     end
-    it 'should select the Check Out template for rendering' do
-      Bike.stub(:find_by_id).with(10).and_return(@fake_bike)
-      @fake_bike.should_receive(:is_available?).and_return(true)
-      get :check_out, {id: 10}
-      response.should render_template('che_ckout')
+    describe 'after verification' do
+      before :each do
+        Bike.stub(:find_by_id).with(10).and_return(@fake_bike)
+        @fake_bike.stub(:is_available?).and_return(true)
+        get :check_out, {id: 10}
+      end
+      it 'should select the Check Out template for rendering' do
+        response.should render_template('che_ckout')
+      end
+      it 'should make the bike information available for the checkout page template' do
+        assigns(:bike).should == @fake_bike
+      end
     end
-    it 'should make the bike information available for the checkout page template'
   end
 end
