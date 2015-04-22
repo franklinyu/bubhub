b8e48dff9e466041f364fc7cf25d8f5911ee8ad9
Franklin Yu <franklinyu@hotmail.com>
2015-04-22 17:14:39 -0400

    merge story *select* and *checkout*

--- spec/controllers/bikes_controller_spec.rb
+++ spec/controllers/bikes_controller_spec.rb
@@ -1,16 +1,16 @@
 require 'spec_helper'
 
 describe BikesController do
-  describe 'select bike' do
+  describe 'check out a bike' do
     before :each do
       @fake_bike = mock('bike 1')
     end
     it 'should check the database for the availability for the bike with model method' do
       Bike.should_receive(:find_by_id).with(10).and_return(@fake_bike)
       @fake_bike.should_receive(:is_available?).and_return(true)
-      post :select, {id: 10}
+      get :checkout, {id: 10}
     end
-    it 'should redirect user to the checkout page'
+    it 'should select the Check Out template for rendering'
     it 'should make the bike information available for the checkout page template'
   end
 end
