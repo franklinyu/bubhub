eaf6c37b5a032a510f729dbbe2e1803c694e607e
Jared Langenauer <jlangen1@binghamton.edu>
2015-04-22 21:08:20 -0300

    Sign In checks email and pin instead of first name. Also stores first/last name into session hash

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -47,13 +47,14 @@ class WelcomeController < ApplicationController
   def valid_user
     path = sign_in_page_path
     @user_list = User.all
-    first_name = params[:user][:first_name]
+    email = params[:user][:email]
     pw = params[:user][:pin]
-    session[:user_name] = first_name
+    session[:email] = email
     params.permit!
     @user_list.each do |u|
-      if first_name == u[:first_name] and pw == u[:pin]
+      if email == u[:email] and pw == u[:pin]
         path = valid_user_page_path
+        session[:first_name] = u[:first_name]
         session[:last_name] = u[:last_name]
         return
       else
