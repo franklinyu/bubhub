cf795d7f1fcecd310324e05f7afbb67e78b9e76e
Franklin Yu <franklinyu@hotmail.com>
2015-04-27 16:12:52 -0400

    fix 'is_available?'; reverse the boolean value

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -1,7 +1,7 @@
 class Bike < ActiveRecord::Base
   after_initialize :default_values
   def is_available?
-    location_id == '-1' or location_id == '0'
+    location_id != '-1' and location_id != '0'
   end
 
   private
