15057a92a085420391bd8bb9751ffc608c931663
Benny Mei <meibenny@gmail.com>
2015-05-04 20:49:10 -0300

    added mailer method to send emails to bikeshare

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,11 +1,22 @@
 # app/mailers/user_mailer.rb
 class UserMailer < ApplicationMailer
 
-default from: 'donotreplybubikeshare@gmail.com'
+@from_bikeshare = 'donotreplybubikeshare@gmail.com'
+@send_to_bikeshare = 'meibenny@gmail.com'
+default from: @bikeshare_email
+
  
   def welcome_email(user)
     @user = user
     #@url  = 'http://example.com/login'
     mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+  end
+
+  def contact_us_email_form(user, contact_us_email_form)
+    @user = user
+    @form = contact_us_email_form
+    subject = ['message from', @form[:name]].join(' ')
+    mail(to: @send_to_bikeshare, reply_to: @form[:email], from: @form[:email], subject: subject)
+
   end
 end
