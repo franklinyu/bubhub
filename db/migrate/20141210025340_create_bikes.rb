25fd2826074af25d6b47954a05a7f23a819c0386
Franklin Yu <franklinyu@hotmail.com>
2015-05-15 15:20:15 -0300

    reverse dulplicated database migration

--- db/migrate/20141210025340_create_bikes.rb
+++ db/migrate/20141210025340_create_bikes.rb
@@ -4,11 +4,6 @@ class CreateBikes < ActiveRecord::Migration
       t.string :bike_id
       t.string :bike_serial_num
       t.string :location_id
-      t.string :bike_serial_num
-      t.string :bike_height
-      t.string :bike_description
-      t.string :last_checkout_id
-      t.timestamp :last_checked_out
       t.timestamps
     end
   end
